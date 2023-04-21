886eb104b98bb8f49c14f9bb8981029d25a85fb7
SangNX <matterhorn@generative.xyz>
2023-04-21 14:11:52 +0700

    chore: send tokens

--- src/modules/Wallet/TokensProfile/index.tsx
+++ src/modules/Wallet/TokensProfile/index.tsx
@@ -1,66 +1,176 @@
 import Empty from '@/components/Empty';
 import Table from '@/components/Table';
 import { TRUSTLESS_COMPUTER_CHAIN_INFO } from '@/constants/chains';
-import { getTokensByWallet } from '@/services/token-explorer';
+import { getTokensWallet } from '@/services/profile';
+import { getUserSelector } from '@/state/user/selector';
 import { decimalToExponential } from '@/utils/format';
-import { getApiKey } from '@/utils/swr';
-import { useWeb3React } from '@web3-react/core';
+import { debounce } from 'lodash';
+import { useEffect, useState } from 'react';
 import { Spinner } from 'react-bootstrap';
-import useSWR from 'swr';
+import InfiniteScroll from 'react-infinite-scroll-component';
+import { useSelector } from 'react-redux';
 import { StyledTokenProfile } from './TokenProfile.styled';
+import TransferModal from './TransferModal';
+import useContractOperation from '@/hooks/contract-operations/useContractOperation';
+import useGetTokenBalance, { IGetTokenBalance } from '@/hooks/contract-operations/token/useGetTokenBalance';
+import { IToken } from '@/interfaces/token';
 
 const EXPLORER_URL = TRUSTLESS_COMPUTER_CHAIN_INFO.explorers[0].url;
 
+const LIMIT_PAGE = 50;
+
 const TokensProfile = () => {
-  const { account } = useWeb3React();
+  const user = useSelector(getUserSelector);
 
-  const profileWallet = account || '';
+  // const {run : getTokenBalance} = useContractOperation
 
-  const TABLE_HEADINGS = ['Token number', 'Name', 'Symbol', 'Supply', ''];
+  const { run: getTokenBalance } = useContractOperation<IGetTokenBalance, string>({
+    operation: useGetTokenBalance,
+    inscribeable: false,
+  });
 
-  const { data, isLoading } = useSWR(getApiKey(getTokensByWallet, { key: profileWallet }), () =>
-    getTokensByWallet({ key: profileWallet }),
+  const profileWallet = user?.walletAddress || '';
+  const [isFetching, setIsFetching] = useState(false);
+  const [showTransferModal, setShowTransferModal] = useState(false);
+  const [selectedToken, setSelectedToken] = useState<IToken | null>(null);
+  const [tokensList, setTokensList] = useState<IToken[]>([]);
+
+  const TABLE_HEADINGS = ['Token Number', 'Name', 'Symbol', 'Balance', 'Max Supply', ''];
+
+  // const { data, error, isLoading } = useSWR(getApiKey(getTokensByWallet, { key: profileWallet }), () =>
+  //   getTokensByWallet({ key: profileWallet }),
+  // );
+
+  const fetchTokenBalances = async (tokenAddrs: string[]) => {
+    try {
+      setIsFetching(true);
+      const balances = await Promise.all(tokenAddrs.map(addr => getTokenBalance({ erc20TokenAddress: addr })));
+      const newTokenList = tokensList.map((token, index: number) => {
+        return {
+          ...token,
+          balance: (parseInt(balances[index]) / decimalToExponential(token.decimal)).toLocaleString(),
+        };
+      });
+      setTokensList(newTokenList);
+    } catch (err: unknown) {
+      console.log('Failed to fetch token balances: ', err);
+    } finally {
+      setIsFetching(false);
+    }
+  };
+
+  const hanldeOpenTransferModal = (selectedToken: any) => {
+    setShowTransferModal(true);
+    setSelectedToken(selectedToken);
+  };
+
+  const hanldeCloseTransferModal = () => {
+    setShowTransferModal(false);
+    setSelectedToken(null);
+  };
+
+  const fetchTokensOwned = async (page = 1, isFetchMore = false) => {
+    try {
+      setIsFetching(true);
+      const res = await getTokensWallet({ walletAddress: profileWallet, limit: 12, page: page });
+      if (isFetchMore) {
+        setTokensList((prev: any) => [...prev, ...res]);
+      } else {
+        setTokensList(res);
+      }
+    } catch (err: unknown) {
+      console.log('Failed to fetch tokens owned');
+    } finally {
+      setIsFetching(false);
+    }
+  };
+
+  const tokenDatas = tokensList.map(
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    token => {
+      const totalSupply = parseInt(token?.totalSupply) / decimalToExponential(token.decimal);
+      const balance = parseInt(token?.balance) / decimalToExponential(token.decimal);
+      const linkTokenExplorer = `${EXPLORER_URL}/token/${token?.address}`;
+
+      return {
+        id: `token-${token?.address}}`,
+        render: {
+          number: token?.index,
+          name: (
+            <a href={linkTokenExplorer} rel="rel=”noopener noreferrer”" target="_blank">
+              {token?.name || '-'}
+            </a>
+          ),
+
+          symbol: token?.symbol || '-',
+          balance: balance.toLocaleString(),
+          supply: totalSupply.toLocaleString(),
+          action: (
+            <>
+              {balance > 0 && (
+                <div className="owner-actions">
+                  <button onClick={() => hanldeOpenTransferModal(token)} className="transfer-button">
+                    Transfer
+                  </button>
+                </div>
+              )}
+            </>
+          ),
+        },
+      };
+    },
   );
 
-  const tokenDatas =
-    data &&
-    data.length > 0 &&
-    data.map(
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      (token: any, index: number) => {
-        const totalSupply = token?.totalSupply / decimalToExponential(token.decimal);
-        const linkTokenExplorer = `${EXPLORER_URL}/token/${token?.address}`;
+  const onLoadMoreTokens = () => {
+    if (isFetching || tokensList.length % LIMIT_PAGE !== 0) return;
+    const page = Math.floor(tokensList.length / LIMIT_PAGE) + 1;
+    fetchTokensOwned(page, true);
+  };
 
-        return {
-          id: `token-${token?.address}}`,
-          render: {
-            number: index + 1,
-            name: (
-              <a href={linkTokenExplorer} rel="rel=”noopener noreferrer”" target="_blank">
-                {token?.name || '-'}
-              </a>
-            ),
+  const debounceLoadMore = debounce(onLoadMoreTokens, 300);
 
-            symbol: token?.symbol || '-',
-            supply: totalSupply.toLocaleString(),
-          },
-        };
-      },
-    );
+  useEffect(() => {
+    if (user && user.walletAddress) fetchTokensOwned();
+  }, [user]);
 
-  if (!data || data.length === 0 || !profileWallet) {
+  useEffect(() => {
+    setTimeout(() => {
+      if (tokensList && tokensList.length > 0) fetchTokenBalances(tokensList.map((token: any) => token.address));
+    }, 2000);
+  }, []);
+
+  if (!tokensList || tokensList.length === 0 || !profileWallet) {
     return <Empty />;
   }
 
   return (
     <StyledTokenProfile>
-      {isLoading ? (
+      {isFetching ? (
         <div className="loading">
           <Spinner animation="border" variant="primary" />
         </div>
       ) : (
-        <Table tableHead={TABLE_HEADINGS} data={tokenDatas} className={'token-table'} />
+        <InfiniteScroll
+          className="transactions"
+          dataLength={tokensList?.length || 0}
+          hasMore={true}
+          loader={
+            isFetching && (
+              <div className="loading">
+                <Spinner animation="border" variant="primary" />
+              </div>
+            )
+          }
+          next={debounceLoadMore}
+        >
+          <Table tableHead={TABLE_HEADINGS} data={tokenDatas} className={'token-table'} />
+        </InfiniteScroll>
       )}
+      <TransferModal
+        show={showTransferModal}
+        handleClose={hanldeCloseTransferModal}
+        erc20TokenAddress={selectedToken?.address}
+      />
     </StyledTokenProfile>
   );
 };
